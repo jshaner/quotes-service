9968e6febfb95a3130a1c5a52ef99aaf6a7d9c41
Jason Shaner <jshaner@cnm.edu>
2020-07-06 12:03:04 -0600

    Implemented flat interfaces for entity classes.

--- src/main/java/edu/cnm/deepdive/quotes/controller/QuotePageController.java
+++ src/main/java/edu/cnm/deepdive/quotes/controller/QuotePageController.java
@@ -2,14 +2,18 @@ package edu.cnm.deepdive.quotes.controller;
 
 
 import edu.cnm.deepdive.quotes.model.entity.Quote;
-import edu.cnm.deepdive.quotes.model.entity.Source;
 import edu.cnm.deepdive.quotes.model.entity.Tag;
 import edu.cnm.deepdive.quotes.service.QuoteRepository;
+import edu.cnm.deepdive.quotes.service.SourceRepository;
 import edu.cnm.deepdive.quotes.service.TagRepository;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,39 +22,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/tags")
-
-public class TagController {
+@Controller
+@RequestMapping("/quotes")
+public class QuotePageController {
 
   private final QuoteRepository quoteRepository;
+  private final SourceRepository sourceRepository;
   private final TagRepository tagRepository;
 
 
   @Autowired
-  public TagController(QuoteRepository quoteRepository,
+  public QuotePageController(QuoteRepository quoteRepository,
+      SourceRepository sourceRepository,
       TagRepository tagRepository) {
     this.quoteRepository = quoteRepository;
+    this.sourceRepository = sourceRepository;
     this.tagRepository = tagRepository;
   }
 
-  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public Iterable<Tag> get() {
-    return tagRepository.getAllByOrderByNameAsc();
-  }
-
-  @GetMapping(value = "/{id:\\d+}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Tag get(@PathVariable long id) {
-    return tagRepository.findById(id)
-        .orElseThrow(NoSuchElementException::new);
+  @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+  public String get(Model model) {
+    model.addAttribute("quotes", quoteRepository.getAllByOrderByTextAsc());
+    return "list";
   }
 
 
-  @GetMapping(value = "/{id:\\d+}/quotes", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Iterable<Quote> getQuotes(@PathVariable long id) {
-    return tagRepository.findById(id)
-        .map((tag) -> quoteRepository.getAllByTagsContainingOrderByTextAsc(tag))
-        .orElseThrow(() -> new NoSuchElementException());
+//  @GetMapping(value = "/{id:\\d+}", produces = MediaType.TEXT_HTML_VALUE)
+//  public String get(@PathVariable long id, Model model) {
+//    return quoteRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+//
+//  }
+
+
+
   }
-}
 
--- src/main/java/edu/cnm/deepdive/quotes/model/entity/Tag.java
+++ src/main/java/edu/cnm/deepdive/quotes/model/entity/Tag.java
@@ -1,22 +1,32 @@
 package edu.cnm.deepdive.quotes.model.entity;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import edu.cnm.deepdive.quotes.view.FlatQuote;
+import edu.cnm.deepdive.quotes.view.FlatTag;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNull;
 
 @SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 
-public class Tag {
+public class Tag implements FlatTag {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -38,18 +48,32 @@ public class Tag {
   @Column(nullable = false, unique = true)
   private String name;
 
+  @ManyToMany(
+      fetch = FetchType.LAZY,
+      mappedBy = "tags",
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
+  )
+
+  @OrderBy("text ASC")
+  @JsonSerialize(contentAs = FlatQuote.class)
+  private List<Quote> quotes = new LinkedList<>();
+
+  @Override
   public Long getId() {
     return id;
   }
 
+  @Override
   public Date getCreated() {
     return created;
   }
 
+  @Override
   public Date getUpdated() {
     return updated;
   }
 
+  @Override
   @NonNull
   public String getName() {
     return name;
@@ -57,5 +81,9 @@ public class Tag {
 
   public void setName(@NonNull String name) {
     this.name = name;
+  }
+
+  public List<Quote> getQuotes() {
+    return quotes;
   }
 }
